588c6bc46c94c050b10605cd41499a07f14f8751
moazkhan05 <48621157+moazkhan05@users.noreply.github.com>
2020-11-04 02:48:22 +0500

    AddTransScreen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from './src/Components/HomeScreen';
+import AddTransaction from './src/Components/AddTransaction';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddTransaction} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text , View } from "react-native";
+
+function AddTransaction() {
+    return (
+        <View>
+            <Text>Add Transaction</Text>
+        </View>
+    )
+}
+
+
+export default AddTransaction;
